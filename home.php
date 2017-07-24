1db446bf6287d324aea7e4ec490c38ed6bcb4e5a
Rachel <rachelpurba96@gmail.com>
2017-07-24 15:01:42 +0700

    rachel nyoba push

--- home.php
+++ home.php
@@ -46,7 +46,7 @@ include ('connect.php'); //connect ke database
           <!-- top tiles -->
           <div class="row tile_count">
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+              <span class="count_top"><i class="fa fa-user"></i> Total Pengguna</span>
               <div class="count">2500</div>
               <span class="count_bottom"><i class="green">4% </i> From last Week</span>
             </div>
