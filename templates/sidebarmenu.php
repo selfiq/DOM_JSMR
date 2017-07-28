4676ebb7575c530da3a4b39bf12c58fadb7b4756
Rachel <rachelpurba96@gmail.com>
2017-07-28 14:59:56 +0700

    dari rachel jumat 28

--- templates/sidebarmenu.php
+++ templates/sidebarmenu.php
@@ -8,20 +8,51 @@
                 </ul>
               <!-- Menu Monitoring Beban dan Capex -->
                   <ul class="nav side-menu">
-                    <li><a><i class="fa fa-book"></i> Beban dan Capex <span class="fa fa-chevron-down"></span></a>
-                      <ul class="nav child_menu">
-                        <li><a href="monitorBeban.php">Beban</a></li>
-                        <li><a>Capex</a></li>
-                      </ul>
-                    </li>
+                   <li><a><i class="fa fa-sitemap"></i> Beban dan Capex <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a>Beban<span class="fa fa-chevron-down"></span></a>
+                          <ul class="nav child_menu">
+                            <li><a>BPT<span class="fa fa-chevron-down"></span></a>
+							  <ul class="nav child_menu">
+								<li><a href="rencana_bpt.php">Rencana</a></li>
+								<li><a href="realisasi_bpt.php">Realisasi</a></li>
+								<li><a href="laporan_bpt.php">Tabel Laporan</a></li>
+							  </ul>
+							</li>
+							<li><a>BPLL<span class="fa fa-chevron-down"></span></a>
+							  <ul class="nav child_menu">
+								<li><a href="rencana_bpll.php">Rencana</a></li>
+								<li><a href="realisasi_bpll.php">Realisasi</a></li>
+								<li><a href="laporan_bpll.php">Tabel Laporan</a></li>
+							  </ul>
+							</li>
+                          </ul>
+                        </li>
+						<li><a>Capex<span class="fa fa-chevron-down"></span></a>
+                          <ul class="nav child_menu">
+                            <li><a>SPOJT<span class="fa fa-chevron-down"></span></a>
+							  <ul class="nav child_menu">
+								<li><a href="#level2_1">Rencana</a></li>
+								<li><a href="#level2_2">Realisasi</a></li>
+								<li class="sub_menu"><a href="level2.html">Tabel Laporan</a></li>
+							  </ul>
+							</li>
+							<li><a>SPJT<span class="fa fa-chevron-down"></span></a>
+							  <ul class="nav child_menu">
+								<li><a href="#level2_1">Rencana</a></li>
+								<li><a href="#level2_2">Realisasi</a></li>
+								<li class="sub_menu"><a href="level2.html">Tabel Laporan</a></li>
+							  </ul>
+							</li>
+                          </ul>
+                        </li>
+                    </ul>
+                  </li>
+				  </li>
                   </ul>
               <!-- Menu Laporan Bulanan -->
                   <ul class="nav side-menu">
-                    <li><a><i class="fa fa-book"></i> Laporan Bulanan <span class="fa fa-chevron-down"></span></a>
-                      <ul class="nav child_menu">
-                        <li><a>Referensi File</a></li>
-                        <li><a>Realisasi</a></li>
-                      </ul>
+                    <li><a><i class="fa fa-book"></i> Laporan Bulanan <span class="fa"></span></a>
                     </li>
                   </ul>
               <!-- Menu Laporan KPI -->
